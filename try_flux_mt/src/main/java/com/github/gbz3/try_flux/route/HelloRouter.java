0169ac62d6f44c12cf59761969989c8b239815b8
gbz3 <gebizo3@gmail.com>
2018-08-18 11:41:13 +0900

    method

--- try_flux_mt/src/main/java/com/github/gbz3/try_flux/route/HelloRouter.java
+++ try_flux_mt/src/main/java/com/github/gbz3/try_flux/route/HelloRouter.java
@@ -1,9 +1,12 @@
 package com.github.gbz3.try_flux.route;
 
-import static org.springframework.web.reactive.function.server.RouterFunctions.*;
-import static org.springframework.web.reactive.function.server.RequestPredicates.*;
-import static org.springframework.web.reactive.function.server.ServerResponse.*;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -16,6 +19,7 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class HelloRouter {
+	private static final Logger logger = LoggerFactory.getLogger( HelloRouter.class );
 	
 	@Autowired
 	HelloRouterProps props;
@@ -23,11 +27,19 @@ public class HelloRouter {
 	public RouterFunction<ServerResponse> routes() {
 		return RouterFunctions.nest( path( props.getRoute() ),
 				route( POST( "/hello" ), this::hello )
+				.andRoute( POST( "/{method}" ), this::handle )
 				);
 	}
 	
 	private Mono<ServerResponse> hello( ServerRequest req ) {
 		return ok().body( Flux.just( "hello", "world" ), String.class );
+	}
+	
+	private Mono<ServerResponse> handle( ServerRequest req ) {
+		final Mono<RequestResource> rr = req.bodyToMono( RequestResource.class );
+		
+		logger.info( "method={}", req.pathVariable( "method" ) );
+		return ok().body( rr, RequestResource.class );
 	}
 
 }
