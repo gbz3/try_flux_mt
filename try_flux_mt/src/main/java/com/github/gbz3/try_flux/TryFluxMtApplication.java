7d52e2be255ba8ba9242bc92a7319b423540d6ac
gbz3 <gebizo3@gmail.com>
2018-08-18 09:23:47 +0900

    init

---
+++ try_flux_mt/src/main/java/com/github/gbz3/try_flux/TryFluxMtApplication.java
@@ -0,0 +1,12 @@
+package com.github.gbz3.try_flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryFluxMtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryFluxMtApplication.class, args);
+	}
+}
---
+++ try_flux_mt/src/test/java/com/github/gbz3/try_flux/TryFluxMtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.gbz3.try_flux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TryFluxMtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
